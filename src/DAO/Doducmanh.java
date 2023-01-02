8eeda2d27c4464372e8581cdebdd90505702048a
Admin <anhcubom1402@gmail.com>
2023-01-02 10:13:37 +0700

    domanhcomit 2

--- src/DAO/Doducmanh.java
+++ src/DAO/Doducmanh.java
@@ -3,5 +3,6 @@ package DAO;
 public class Doducmanh {
     public static void main(String[] args) {
         System.out.println("Xin chào Đỗ Đức Mạnh");
+        System.out.println("Xin chào Mạnh");
     }
 }
